1c5723c70c7bd7a7612269d8e3ff03b33c13bc70
daegyunpark <daegyunpark@daou.co.kr>
2025-03-27 09:26:25 +0900

    create offer

---
+++ offer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+
+typedef struct {
+	int id;
+	char accountNum[20];
+	char ticker[20];
+	int exchangeCode;
+	char status[20];
+	int quantiy;
+	int price;
+	char createdAt[20];
+} OFFER;
+
+//void convert_to_utf8(const wchar_t* wstr, char* utf8str, int utf8str_size);
+void insert_offer(OFFER*);
---
+++ offerService.c
@@ -0,0 +1,32 @@
+#include "Offer.h"  
+#include "OfferService.h"  
+
+
+void add_offer() {
+	OFFER offer;
+	int type;
+	printf("계좌 번호: \n");
+	scanf("%s", offer.accountNum);
+	printf("종목명: \n");
+	scanf("%s", offer.ticker);
+	printf("거래 유형(0:매수, 1:매도): \n");
+	scanf("%d", &type);
+	if (type) strcpy(offer.status, "매수");
+	else strcpy(offer.status, "매도");
+	printf("거래 수량: \n");
+	scanf("%d", &offer.quantiy);
+	printf("거래 가격: \n");
+	scanf("%d", &offer.price);
+
+
+	// 이거 정리해서 넘겨야함
+	offer.exchangeCode = 0;
+
+	insert_offer(&offer);
+	//가격 불러오기
+	//가격 비교 (sor) 
+	// 반대케이스 저장
+	//체결 테이블 작성
+	//보유 주식 변경
+
+}
