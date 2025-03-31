9535c2b6a90f6d40e38233b603f588c192812708
daegyunpark <daegyunpark@daou.co.kr>
2025-03-31 09:03:38 +0900

    trade history

--- offer.h
+++ offer.h
@@ -12,8 +12,11 @@ typedef struct {
 	char status[20];
 	int quantiy;
 	int price;
-	char createdAt[20];
+	char createdAt[50];
 } OFFER;
 
 //void convert_to_utf8(const wchar_t* wstr, char* utf8str, int utf8str_size);
 int insert_offer(OFFER*);
+int count_offers(char* accountNum);
+void get_offers(const char* accountNum, OFFER *, int count);
+void check_trade(OFFER*);
